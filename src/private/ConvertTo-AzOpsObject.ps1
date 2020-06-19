3f09f76234eddcacd637c6ed539eb685f8c68219
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2020-06-19 11:52:59 +0100

    Logging review (#18)
    * Update Write-X commands, comments and spelling
    
    * Revert Write-AzOpsLog to Write-Verbose
    
    Tests appear to fail due to not supporting custom Write-AzOpsLog function.
    
    * Enable pipeline input and set Timestamp to switch
    
    * Add $Global:AzOpsLogTimestampPreference variable
    
    * Improve logTimeUtc format for readability

--- src/private/ConvertTo-AzOpsObject.ps1
+++ src/private/ConvertTo-AzOpsObject.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
     This cmdlet serializes and, depending on the input, sorts powershell objects for deterministic output.
 .EXAMPLE
-    #Convert the object to [PSCustomObject] without ordering to be able to alter/change all properties
+    # Convert the object to [PSCustomObject] without ordering to be able to alter/change all properties
     $subscription = Get-AzSubscription | Select-Object -First 1
     $subscription | ConvertTo-AzOpsObject
 
@@ -17,7 +17,7 @@
     ExtendedProperties        : @{Environment=AzureCloud; Account=96657a67-755f-4f93-8b8e-317f9a107323; Tenants=3fc1081d-6105-4e19-b60c-1ec1252cf560}
     CurrentStorageAccount     :
 .EXAMPLE
-    #Convert the object to [PSCustomObject] and order all properties in alphabetical order
+    # Convert the object to [PSCustomObject] and order all properties in alphabetical order
     $subscription = Get-AzSubscription | Select-Object -First 1
     $subscription | ConvertTo-AzOpsObject -OrderObject
 
@@ -39,10 +39,10 @@ function ConvertTo-AzOpsObject {
     [CmdletBinding()]
     [OutputType([PSCustomObject])]
     param (
-        #Inputobject to serialize
+        # Inputobject to serialize
         [Parameter(ValueFromPipeline)]
         $InputObject,
-        #Used if the object properties should be ordered in alphabetical order
+        # Used if the object properties should be ordered in alphabetical order
         [Parameter(Mandatory = $false)]
         [switch]$OrderObject
     )
@@ -50,15 +50,15 @@ function ConvertTo-AzOpsObject {
     begin {}
 
     process {
-        #If [switch]$OrderObject has been used, go through each property recursively to ensure it is sorted/ordered properly
+        # if [switch]$OrderObject has been used, go through each property recursively to ensure it is sorted/ordered properly
         if ($PSBoundParameters["OrderObject"]) {
-            #Check input object type
+            # Check input object type
             if ($InputObject -is [PSObject] -or [PSCustomObject] -and $InputObject -isnot [string] -and $inputobject -isnot [array] -and $InputObject -isnot [System.Collections.ICollection] -and $inputobject -isnot [Microsoft.Azure.Commands.ResourceManager.Cmdlets.Implementation.Policy.PsPolicyType]) {
-                #Create ordered PSCustomObject
+                # Create ordered PSCustomObject
                 $Object = [PSCustomObject][ordered]@{ }
-                #Sort properties
+                # Sort properties
                 $Properties = $InputObject.PSObject.Properties | Sort-Object -Property Name
-                #Loop through properties recursively
+                # Loop through properties recursively
                 foreach ($property in $Properties ) {
                     # $value = ConvertTo-AzOpsObject -InputObject $property.Value -OrderObject:$OrderObject
                     # Add-Member -InputObject $Object -Name $property.Name -Value $value -MemberType NoteProperty -Force
@@ -72,7 +72,7 @@ function ConvertTo-AzOpsObject {
                 }
                 Write-Output -NoEnumerate -InputObject $Object
             }
-            #Handle hash tables and dictionaries
+            # Handle hash tables and dictionaries
             elseif ($InputObject -is [System.Collections.ICollection] -and $InputObject -isnot [string] -and $inputobject -isnot [array]) {
                 $hash = [ordered]@{ }
                 $keys = ($Inputobject.Keys | Sort-Object)
@@ -81,29 +81,29 @@ function ConvertTo-AzOpsObject {
                 }
                 $hash
             }
-            #Handle arrays
+            # Handle arrays
             elseif ($InputObject -is [array]) {
 
                 if ($InputObject.count -eq 0) {
-                    #Return empty array if empty array (powershell will by default return $null)
+                    # Return empty array if empty array (powershell will by default return $null)
                     return , @()
                 }
                 elseif ($InputObject.count -eq 1) {
-                    #Return array with count 1 as array (powershell will automatically convert it to a string)
+                    # Return array with count 1 as array (powershell will automatically convert it to a string)
                     return , @($InputObject)
                 }
                 else {
-                    #Return the array
+                    # Return the array
                     $InputObject | Sort-Object
                 }
             }
             else {
-                #If the object isn't a collection, hash table or other psobject - it is a string
+                # if the object isn't a collection, hash table or other psobject - it is a string
                 $InputObject | Sort-Object
             }
         }
         else {
-            #If [switch]$OrderObject has not been used, route the object through ConvertTo/ConvertFrom-Json to create a PSCustomObject to be able to modify/remove the properties
+            # if [switch]$OrderObject has not been used, route the object through ConvertTo/ConvertFrom-Json to create a PSCustomObject to be able to modify/remove the properties
             $InputObject | ConvertTo-Json -Depth 100 | ConvertFrom-Json -Depth 100
         }
     }
