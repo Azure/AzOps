5c297988f3d2656e1a03de47f41948ab96e8b1a4
Lyon Till <lyon.till@microsoft.com>
2020-06-23 23:31:18 +0100

    Update commit

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -21,7 +21,7 @@ function Initialization {
 
     begin {
         try {
-            # Create [pscredential]
+            # Create credential
             $credentials = ($env:AZURE_CREDENTIALS | ConvertFrom-Json)
             $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $credentials.clientId, ($credentials.clientSecret | ConvertTo-SecureString -AsPlainText -Force)
 
@@ -33,38 +33,32 @@ function Initialization {
                 git config --global user.name $env:GITHUB_USERNAME
             } | Out-Host
 
-            if($env:AZOPS_STATE)
-            {
+            # Set environment variables
+            if ($env:AZOPS_STATE) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting AzOpsState to $($env:AZOPS_STATE)"
                 $env:AzOpsState = $env:AZOPS_STATE
             }
-            if($env:AZOPS_ENROLLMENT_ACCOUNT_PRINCIPAL_NAME)
-            {
+            if ($env:AZOPS_ENROLLMENT_ACCOUNT_PRINCIPAL_NAME) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting AzOpsEnrollmentAccountPrincipalName to $($env:AZOPS_ENROLLMENT_ACCOUNT_PRINCIPAL_NAME)"
-                $env:AzOpsEnrollmentAccountPrincipalName = $env:AZOPS_STATE
+                $env:AzOpsEnrollmentAccountPrincipalName = $env:AZOPS_ENROLLMENT_ACCOUNT_PRINCIPAL_NAME
             }
-            if($env:AZOPS_OFFER_TYPE)
-            {
+            if ($env:AZOPS_OFFER_TYPE) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting offerType to $($env:AZOPS_OFFER_TYPE)"
                 $env:offerType = $env:AZOPS_OFFER_TYPE
             }
-            if($env:AZOPS_DEFAULT_DEPLOYMENT_REGION)
-            {
+            if ($env:AZOPS_DEFAULT_DEPLOYMENT_REGION) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting AzOpsDefaultDeploymentRegion to $($env:AZOPS_DEFAULT_DEPLOYMENT_REGION)"
                 $env:AzOpsDefaultDeploymentRegion = $env:AZOPS_DEFAULT_DEPLOYMENT_REGION
             }
-            if($env:AZOPS_INVALIDATE_CACHE)
-            {
+            if ($env:AZOPS_INVALIDATE_CACHE) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting InvalidateCache to $($env:AZOPS_INVALIDATE_CACHE)"
                 $env:InvalidateCache = $env:AZOPS_INVALIDATE_CACHE
             }
-            if($env:AZOPS_IGNORE_CONTEXT_CHECK)
-            {
+            if ($env:AZOPS_IGNORE_CONTEXT_CHECK) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting IgnoreContextCheck to $($env:AZOPS_IGNORE_CONTEXT_CHECK)"
                 $env:IgnoreContextCheck = $env:AZOPS_IGNORE_CONTEXT_CHECK
             }
-            if($env:AZOPS_THROTTLE_LIMIT)
-            {
+            if ($env:AZOPS_THROTTLE_LIMIT) {
                 Write-AzOpsLog -Level Information -Topic "pwsh" -Message "Setting AzOpsThrottleLimit to $($env:AZOPS_THROTTLE_LIMIT)"
                 $env:AzOpsThrottleLimit = $env:AZOPS_THROTTLE_LIMIT
             }
