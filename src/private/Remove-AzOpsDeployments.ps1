d6ac6cde7c8b0b5bcf0a01ad7bedbdbb33e9a6d6
uday31in <14359777+uday31in@users.noreply.github.com>
2020-06-26 08:46:58 +0100

    adding function to remove deployments

---
+++ src/private/Remove-AzOpsDeployments.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    This cmdlets removes all Deployments.
+.DESCRIPTION
+    This cmdlets removes all Deployments.
+.EXAMPLE
+    Remove-AzOpsDeployments
+.INPUTS
+    None.
+.OUTPUTS
+    None.
+#>
+function Remove-AzOpsDeployments {
+    Get-AzTenantDeployment   | Foreach-Object -Parallel {
+        Write-Host "$(Get-Date) Removing Deployment $($_.Id)"
+        Remove-AzTenantDeployment -DeploymentName $_.DeploymentName
+    }
+
+    Get-AzManagementGroup   | Foreach-Object -Parallel {
+        Get-AzManagementGroupDeployment -ManagementGroupId $_.Name |  Foreach-Object -Parallel {
+            Write-Host "$(Get-Date) Removing Deployment $($_.Id)"
+            Remove-AzManagementGroupDeployment -DeploymentName $_.DeploymentName -ManagementGroupId $_.ManagementGroupId
+        }
+    }
+
+    Get-AzSubscription | ForEach-Object {
+        Set-AzContext -SubscriptionId $_.SubscriptionId | out-null
+        Get-AzSubscriptionDeployment |  Foreach-Object -Parallel {
+            Write-Host "$(Get-Date) Removing Deployment $($_.Id)"
+            Remove-AzSubscriptionDeployment -Id $_.Id
+        }
+    }
+}
