47f4081187662d6304455a296a0233a3944aee53
Lyon Till <lyon.till@microsoft.com>
2021-02-04 12:49:29 +0000

    Remove build/
    Remove unused files

--- build/vsts-validate.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Guide for available variables and working with secrets:
-# https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
-
-# Needs to ensure things are Done Right and only legal commits to master get built
-
-# Run internal pester tests
-& "$PSScriptRoot\..\AzOps\tests\pester.ps1"
