34a11f988eebaae95c4b13450b546b1fd0345984
Lyon Till <158992+ljtill@users.noreply.github.com>
2021-02-21 11:51:57 +0000

    Update functions/

--- src/internal/functions/Assert-AzOpsWindowsLongPath.ps1
+++ src/internal/functions/Assert-AzOpsWindowsLongPath.ps1
@@ -1,4 +1,4 @@
-﻿function Assert-WindowsLongPath {
+﻿function Assert-AzOpsWindowsLongPath {
 
     <#
         .SYNOPSIS
@@ -8,7 +8,7 @@
         .PARAMETER Cmdlet
             The $PSCmdlet variable of the calling command.
         .EXAMPLE
-            > Assert-WindowsLongPath -Cmdlet $PSCmdlet
+            > Assert-AzOpsWindowsLongPath -Cmdlet $PSCmdlet
             Asserts that - if on windows - long paths have been enabled.
     #>
 
@@ -23,7 +23,7 @@
             return
         }
 
-        Write-PSFMessage -Level InternalComment -String 'Assert-WindowsLongPath.Validating'
+        Write-PSFMessage -Level InternalComment -String 'Assert-AzOpsWindowsLongPath.Validating'
         $hasRegKey = 1 -eq (Get-ItemPropertyValue -Path HKLM:SYSTEM\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled)
         $hasGitConfig = (Invoke-NativeCommand -ScriptBlock { git config --system -l } -IgnoreExitcode | Select-String 'core.longpaths=true') -as [bool]
 
@@ -31,15 +31,15 @@
             return
         }
         if (-not $hasRegKey) {
-            Write-PSFMessage -Level Warning -String 'Assert-WindowsLongPath.No.Registry'
+            Write-PSFMessage -Level Warning -String 'Assert-AzOpsWindowsLongPath.No.Registry'
         }
         if (-not $hasGitConfig) {
-            Write-PSFMessage -Level Warning -String 'Assert-WindowsLongPath.No.GitCfg'
+            Write-PSFMessage -Level Warning -String 'Assert-AzOpsWindowsLongPath.No.GitCfg'
         }
 
         $exception = [System.InvalidOperationException]::new('Windows not configured for long paths. Please follow instructions for "Enabling long paths on Windows" on https://aka.ms/es/quickstart.')
         $errorRecord = [System.Management.Automation.ErrorRecord]::new($exception, "ConfigurationError", 'InvalidOperation', $null)
-        Write-PSFMessage -Level Warning -String 'Assert-WindowsLongPath.Failed' -Tag error
+        Write-PSFMessage -Level Warning -String 'Assert-AzOpsWindowsLongPath.Failed' -Tag error
         $Cmdlet.ThrowTerminatingError($errorRecord)
     }
 
