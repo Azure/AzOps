14b5fa1d4e6fe0c2555583f89b8e8380c0a419de
Lyon Till <lyon.till@microsoft.com>
2021-03-23 15:28:20 +0000

    Update Debug.ps1
    Refine default debug logic

--- scripts/Debug.ps1
+++ scripts/Debug.ps1
@@ -1,15 +1,33 @@
+#
+# Preferences
+#
 
-$DebugPreference = "Continue"
-$VerbosePreference = "Continue"
+#$DebugPreference = "Continue"
+#$VerbosePreference = "Continue"
 
-Set-PSFConfig -FullName AzOps.Core.State -Value "/tmp/azops" -Description "-"
-Set-PSFConfig -FullName AzOps.Import.DoDotSource -Value $true
-Set-PSFConfig -FullName AzOps.Import.IndividualFiles -Value $true
+#
+# Config
+#
+
+Set-PSFConfig -FullName PSFramework.Message.Info.Maximum -Value 9
+Set-PSFConfig -FullName AzOps.Core.State -Value "/workspaces/azops"
+#Set-PSFConfig -FullName AzOps.Import.DoDotSource -Value $true
+#Set-PSFConfig -FullName AzOps.Import.IndividualFiles -Value $true
+
+#
+# Import
+#
 
 Import-Module ./src/AzOps.psd1 -Force
 
-#Initialize-AzOpsEnvironment
+#
+# Initialize
+#
 
-#Remove-Item -Path "/tmp/azops" -Recurse -Force
+Initialize-AzOpsRepository
 
-#Initialize-AzOpsRepository -Verbose
+#
+# Internal
+#
+
+& ( Get-Module -Name AzOps ) { $host.EnterNestedPrompt() }
