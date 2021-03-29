308c059eab477729645957ce38ee8845c0e11292
Lyon Till <158992+ljtill@users.noreply.github.com>
2021-03-29 18:28:48 +0100

    Tests (#255)
    * Update .github/
    
    * Update scripts/
    
    * Delete .pipelines/
    
    * Update .github/
    
    * Update .github/
    
    * Update .github/

--- scripts/Tests.ps1
+++ scripts/Tests.ps1
@@ -1,7 +1,7 @@
 # Guide for available variables and working with secrets:
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
-# Needs to ensure things are Done Right and only ethical commits to main get built
+# Needs to ensure things are Done Right and only working commits to main get built
 
 # Run internal pester tests
 & "$PSScriptRoot\..\src\tests\Pester.ps1"
