cdaccbabceed054b75f80f6344ff66160aa58a2a
Lyon Till <lyon.till@microsoft.com>
2021-04-09 21:41:14 +0000

    Update Strings.Tests.ps1 Fix filename casing

--- src/tests/general/Strings.Tests.ps1
+++ src/tests/general/Strings.Tests.ps1
@@ -11,7 +11,7 @@
 Describe "Testing localization strings" {
     $moduleRoot = (Get-Module AzOps).ModuleBase
     $stringsResults = Export-PSMDString -ModuleRoot $moduleRoot
-    $exceptions = & "$global:testroot\general\strings.Exceptions.ps1"
+    $exceptions = & "$global:testroot\general\Strings.Exceptions.ps1"
 
     foreach ($stringEntry in $stringsResults) {
         if ($stringEntry.String -eq "key") { continue } # Skipping the template default entry
