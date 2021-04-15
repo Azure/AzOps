05a3f0a17d3e3067d5142b93722394d2091982d7
Lyon Till <lyon.till@microsoft.com>
2021-04-15 14:50:24 +0000

    Update Dependencies.ps1 Switch to Write-Output

--- scripts/Dependencies.ps1
+++ scripts/Dependencies.ps1
@@ -6,14 +6,14 @@ param (
 # Runtime Modules
 $data = Import-PowerShellDataFile -Path "$PSScriptRoot/../src/AzOps.psd1"
 foreach ($dependency in $data.RequiredModules) {
-    Write-Host "Installing module $($dependency.ModuleName) $($dependency.RequiredVersion)"
+    Write-Output "Installing module $($dependency.ModuleName) $($dependency.RequiredVersion)"
     Install-Module -Name $dependency.ModuleName -RequiredVersion $dependency.RequiredVersion -Repository $Repository -Force
 }
 
 # Development Modules
 $modules = @("Pester", "PSModuleDevelopment", "PSScriptAnalyzer")
 foreach ($module in $modules) {
-    Write-Host "Installing module $module"
+    Write-Output "Installing module $module"
     Install-Module $module -Repository $Repository -Force
 }
 
