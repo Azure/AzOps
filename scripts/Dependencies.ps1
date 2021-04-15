4f2e6c6622058075f708a993831f54474aa483ef
Lyon Till <lyon.till@microsoft.com>
2021-04-15 14:59:30 +0000

    Update Dependencies.ps1

--- scripts/Dependencies.ps1
+++ scripts/Dependencies.ps1
@@ -6,14 +6,12 @@ param (
 # Runtime Modules
 $data = Import-PowerShellDataFile -Path "$PSScriptRoot/../src/AzOps.psd1"
 foreach ($dependency in $data.RequiredModules) {
-    Write-Output "Installing module $($dependency.ModuleName) $($dependency.RequiredVersion)"
     Install-Module -Name $dependency.ModuleName -RequiredVersion $dependency.RequiredVersion -Repository $Repository -Force
 }
 
 # Development Modules
 $modules = @("Pester", "PSModuleDevelopment", "PSScriptAnalyzer")
 foreach ($module in $modules) {
-    Write-Output "Installing module $module"
     Install-Module $module -Repository $Repository -Force
 }
 
