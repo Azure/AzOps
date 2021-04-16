bb3bc458230bbdf8e90320eff524fa1660062d49
Lyon Till <lyon.till@microsoft.com>
2021-04-16 09:12:27 +0000

    Update Pester.ps1

--- src/tests/Pester.ps1
+++ src/tests/Pester.ps1
@@ -6,7 +6,7 @@
     $TestFunctions = $false,
 
     [ValidateSet('None', 'Normal', 'Detailed', 'Diagnostic')]
-    $Output = "Detailed",
+    $Output = "Normal",
 
     $Include = "*",
 
@@ -21,6 +21,7 @@ $global:testroot = $PSScriptRoot
 $global:__pester_data = @{ }
 
 Remove-Module AzOps -ErrorAction Ignore
+
 Import-Module "$PSScriptRoot\..\AzOps.psd1" -Scope Global
 Import-Module "$PSScriptRoot\..\AzOps.psm1" -Scope Global -Force
 
@@ -67,66 +68,66 @@ if ($TestIntegration) {
 #endregion Run Integration Tests
 
 #region Run General Tests
-if ($TestGeneral) {
-    Write-PSFMessage -Level Important -Message "Proceeding with general tests"
-    foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1")) {
-        if ($file.Name -notlike $Include) { continue }
-        if ($file.Name -like $Exclude) { continue }
+# if ($TestGeneral) {
+#     Write-PSFMessage -Level Important -Message "Proceeding with general tests"
+#     foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1")) {
+#         if ($file.Name -notlike $Include) { continue }
+#         if ($file.Name -like $Exclude) { continue }
 
-        Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
-        $config.Run.Path = $file.FullName
-        $config.Run.PassThru = $true
-        $config.Output.Verbosity = $Output
-        $results = Invoke-Pester -Configuration $config
-        foreach ($result in $results) {
-            $totalRun += $result.TotalCount
-            $totalFailed += $result.FailedCount
-            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
-                $testresults += [pscustomobject]@{
-                    Block   = $_.Block
-                    Name    = "It $($_.Name)"
-                    Result  = $_.Result
-                    Message = $_.ErrorRecord.DisplayErrorMessage
-                }
-            }
-        }
-    }
-}
+#         Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
+#         $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
+#         $config.Run.Path = $file.FullName
+#         $config.Run.PassThru = $true
+#         $config.Output.Verbosity = $Output
+#         $results = Invoke-Pester -Configuration $config
+#         foreach ($result in $results) {
+#             $totalRun += $result.TotalCount
+#             $totalFailed += $result.FailedCount
+#             $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
+#                 $testresults += [pscustomobject]@{
+#                     Block   = $_.Block
+#                     Name    = "It $($_.Name)"
+#                     Result  = $_.Result
+#                     Message = $_.ErrorRecord.DisplayErrorMessage
+#                 }
+#             }
+#         }
+#     }
+# }
 #endregion Run General Tests
 
 $global:__pester_data.ScriptAnalyzer | Out-Host
 
 #region Test Commands
-if ($TestFunctions) {
-    Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
-    foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*.Tests.ps1")) {
-        if ($file.Name -notlike $Include) { continue }
-        if ($file.Name -like $Exclude) { continue }
+# if ($TestFunctions) {
+#     Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+#     foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*.Tests.ps1")) {
+#         if ($file.Name -notlike $Include) { continue }
+#         if ($file.Name -like $Exclude) { continue }
 
-        Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
-        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
-        $config.Run.Path = $file.FullName
-        $config.Run.PassThru = $true
-        $config.Output.Verbosity = $Output
-        $results = Invoke-Pester -Configuration $config
-        foreach ($result in $results) {
-            $totalRun += $result.TotalCount
-            $totalFailed += $result.FailedCount
-            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
-                $testresults += [pscustomobject]@{
-                    Block   = $_.Block
-                    Name    = "It $($_.Name)"
-                    Result  = $_.Result
-                    Message = $_.ErrorRecord.DisplayErrorMessage
-                }
-            }
-        }
-    }
-}
+#         Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
+#         $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
+#         $config.Run.Path = $file.FullName
+#         $config.Run.PassThru = $true
+#         $config.Output.Verbosity = $Output
+#         $results = Invoke-Pester -Configuration $config
+#         foreach ($result in $results) {
+#             $totalRun += $result.TotalCount
+#             $totalFailed += $result.FailedCount
+#             $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
+#                 $testresults += [pscustomobject]@{
+#                     Block   = $_.Block
+#                     Name    = "It $($_.Name)"
+#                     Result  = $_.Result
+#                     Message = $_.ErrorRecord.DisplayErrorMessage
+#                 }
+#             }
+#         }
+#     }
+# }
 #endregion Test Commands
 
-$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
+#$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
 
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
