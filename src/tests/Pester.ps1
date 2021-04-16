089bccc50f8c7e19d103902d822258366967eb94
Lyon Till <lyon.till@microsoft.com>
2021-04-16 10:55:10 +0000

    Update Pester.ps1

--- src/tests/Pester.ps1
+++ src/tests/Pester.ps1
@@ -68,64 +68,64 @@ if ($TestIntegration) {
 #endregion Run Integration Tests
 
 #region Run General Tests
-# if ($TestGeneral) {
-#     Write-PSFMessage -Level Important -Message "Proceeding with general tests"
-#     foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1")) {
-#         if ($file.Name -notlike $Include) { continue }
-#         if ($file.Name -like $Exclude) { continue }
+if ($TestGeneral) {
+    Write-PSFMessage -Level Important -Message "Proceeding with general tests"
+    foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1")) {
+        if ($file.Name -notlike $Include) { continue }
+        if ($file.Name -like $Exclude) { continue }
 
-#         Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-#         $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
-#         $config.Run.Path = $file.FullName
-#         $config.Run.PassThru = $true
-#         $config.Output.Verbosity = $Output
-#         $results = Invoke-Pester -Configuration $config
-#         foreach ($result in $results) {
-#             $totalRun += $result.TotalCount
-#             $totalFailed += $result.FailedCount
-#             $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
-#                 $testresults += [pscustomobject]@{
-#                     Block   = $_.Block
-#                     Name    = "It $($_.Name)"
-#                     Result  = $_.Result
-#                     Message = $_.ErrorRecord.DisplayErrorMessage
-#                 }
-#             }
-#         }
-#     }
-# }
+        Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
+        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
+        $config.Run.Path = $file.FullName
+        $config.Run.PassThru = $true
+        $config.Output.Verbosity = $Output
+        $results = Invoke-Pester -Configuration $config
+        foreach ($result in $results) {
+            $totalRun += $result.TotalCount
+            $totalFailed += $result.FailedCount
+            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
+                $testresults += [pscustomobject]@{
+                    Block   = $_.Block
+                    Name    = "It $($_.Name)"
+                    Result  = $_.Result
+                    Message = $_.ErrorRecord.DisplayErrorMessage
+                }
+            }
+        }
+    }
+}
 #endregion Run General Tests
 
 $global:__pester_data.ScriptAnalyzer | Out-Host
 
-#region Test Commands
-# if ($TestFunctions) {
-#     Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
-#     foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*.Tests.ps1")) {
-#         if ($file.Name -notlike $Include) { continue }
-#         if ($file.Name -like $Exclude) { continue }
+#region Function Tests
+if ($TestFunctions) {
+    Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+    foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*.Tests.ps1")) {
+        if ($file.Name -notlike $Include) { continue }
+        if ($file.Name -like $Exclude) { continue }
 
-#         Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
-#         $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
-#         $config.Run.Path = $file.FullName
-#         $config.Run.PassThru = $true
-#         $config.Output.Verbosity = $Output
-#         $results = Invoke-Pester -Configuration $config
-#         foreach ($result in $results) {
-#             $totalRun += $result.TotalCount
-#             $totalFailed += $result.FailedCount
-#             $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
-#                 $testresults += [pscustomobject]@{
-#                     Block   = $_.Block
-#                     Name    = "It $($_.Name)"
-#                     Result  = $_.Result
-#                     Message = $_.ErrorRecord.DisplayErrorMessage
-#                 }
-#             }
-#         }
-#     }
-# }
-#endregion Test Commands
+        Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
+        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
+        $config.Run.Path = $file.FullName
+        $config.Run.PassThru = $true
+        $config.Output.Verbosity = $Output
+        $results = Invoke-Pester -Configuration $config
+        foreach ($result in $results) {
+            $totalRun += $result.TotalCount
+            $totalFailed += $result.FailedCount
+            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
+                $testresults += [pscustomobject]@{
+                    Block   = $_.Block
+                    Name    = "It $($_.Name)"
+                    Result  = $_.Result
+                    Message = $_.ErrorRecord.DisplayErrorMessage
+                }
+            }
+        }
+    }
+}
+#region Function Tests
 
 #$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
 
