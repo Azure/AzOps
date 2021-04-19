aef892447ed498f933e5f44e0dd9cf7c2097b7fd
Lyon Till <lyon.till@microsoft.com>
2021-04-19 08:56:26 +0000

    Update Pester.ps1 Modify script ordering

--- src/tests/Pester.ps1
+++ src/tests/Pester.ps1
@@ -1,9 +1,9 @@
 ﻿param (
-    $TestIntegration = $true,
-
-    $TestGeneral = $false,
+    $TestGeneral = $true,
 
     $TestFunctions = $false,
+
+    $TestIntegration = $true,
 
     [ValidateSet('None', 'Normal', 'Detailed', 'Diagnostic')]
     $Output = "None",
@@ -37,35 +37,6 @@ $totalRun = 0
 $testresults = @()
 $config = [PesterConfiguration]::Default
 $config.TestResult.Enabled = $true
-
-#region Run Integration Tests
-if ($TestIntegration) {
-    Write-PSFMessage -Level Important -Message "Proceeding with integration tests"
-    foreach ($file in (Get-ChildItem "$PSScriptRoot\integration" | Where-Object Name -like "*.Tests.ps1")) {
-        if ($file.Name -notlike $Include) { continue }
-        if ($file.Name -like $Exclude) { continue }
-
-        Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
-        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
-        $config.Run.Path = $file.FullName
-        $config.Run.PassThru = $true
-        $config.Output.Verbosity = $Output
-        $results = Invoke-Pester -Configuration $config
-        foreach ($result in $results) {
-            $totalRun += $result.TotalCount
-            $totalFailed += $result.FailedCount
-            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
-                $testresults += [pscustomobject]@{
-                    Block   = $_.Block
-                    Name    = "It $($_.Name)"
-                    Result  = $_.Result
-                    Message = $_.ErrorRecord.DisplayErrorMessage
-                }
-            }
-        }
-    }
-}
-#endregion Run Integration Tests
 
 #region Run General Tests
 if ($TestGeneral) {
@@ -127,7 +98,38 @@ if ($TestFunctions) {
 }
 #region Function Tests
 
-#$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
+$global:__pester_data.ScriptAnalyzer | Out-Host
+
+#region Run Integration Tests
+if ($TestIntegration) {
+    Write-PSFMessage -Level Important -Message "Proceeding with integration tests"
+    foreach ($file in (Get-ChildItem "$PSScriptRoot\integration" | Where-Object Name -like "*.Tests.ps1")) {
+        if ($file.Name -notlike $Include) { continue }
+        if ($file.Name -like $Exclude) { continue }
+
+        Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
+        $config.TestResult.OutputPath = Join-Path "$PSScriptRoot\..\..\results" "$($file.BaseName).xml"
+        $config.Run.Path = $file.FullName
+        $config.Run.PassThru = $true
+        $config.Output.Verbosity = $Output
+        $results = Invoke-Pester -Configuration $config
+        foreach ($result in $results) {
+            $totalRun += $result.TotalCount
+            $totalFailed += $result.FailedCount
+            $result.Tests | Where-Object Result -ne 'Passed' | ForEach-Object {
+                $testresults += [pscustomobject]@{
+                    Block   = $_.Block
+                    Name    = "It $($_.Name)"
+                    Result  = $_.Result
+                    Message = $_.ErrorRecord.DisplayErrorMessage
+                }
+            }
+        }
+    }
+}
+#endregion Run Integration Tests
+
+$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
 
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
