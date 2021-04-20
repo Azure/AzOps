b19dcb72c396cf3eec8850fac70ada02cc8fe706
Lyon Till <158992+ljtill@users.noreply.github.com>
2021-04-20 21:27:52 +0100

    Create Version.ps1

---
+++ scripts/Version.ps1
@@ -0,0 +1,33 @@
+<#
+This script updates the module manifest version.
+#>
+
+param (
+    [Parameter()]
+    [ValidateSet("Major", "Minor", "Patch")]
+    $Type
+)
+
+#region Updating the Module Version
+if ($Type) {
+    Write-PSFMessage -Level Important -Message "Updating module version numbers."
+
+    [Version]$currentVersion = (Import-PowerShellDataFile -Path "./src/AzOps.psd1").ModuleVersion
+
+    [Version]$releaseVersion = switch($Type) {
+        "Major" {
+            [Version]::new($currentVersion.Major + 1, 0, 0)
+        }
+        "Minor" {
+            $Minor = if($currentVersion.Minor -le 0) { 1 } else { $currentVersion.Minor + 1 }
+            [Version]::new($currentVersion.Major, $Minor, 0)
+        }
+        "Patch" {
+            $Build = if($currentVersion.Build -le 0) { 1 } else { $currentVersion.Build + 1 }
+            [Version]::new($currentVersion.Major, $currentVersion.Minor, $Build)
+        }
+    }
+
+    Update-ModuleManifest -Path "./src/AzOps.psd1" -ModuleVersion $releaseVersion
+}
+#endregion Updating the Module Version
