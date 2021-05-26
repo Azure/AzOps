98f2835380fce35b4da0d145df0de81123037589
Guillaume Bordier <g_bordier@hotmail.com>
2021-05-26 20:03:55 +0200

    Gbordier fixpatch (#2)
    * changed to resourcegroupname
    
    * remove comment
    
    Co-authored-by: Guillaume Bordier <gbordier@microsoft.com>

--- src/internal/functions/New-AzOpsDeployment.ps1
+++ src/internal/functions/New-AzOpsDeployment.ps1
@@ -115,11 +115,11 @@
             }
             else {
                 Write-PSFMessage -String 'New-AzOpsDeployment.ResourceGroup.Processing' -StringValues $scopeObject -Target $scopeObject
-
+                
                 $parameters = @{
                     'TemplateFile'                = $TemplateFilePath
                     'SkipTemplateParameterPrompt' = $true
-                    'ResourceGroupName'           = $scopeObject.Name
+                    'ResourceGroupName'           = $scopeObject.resourcegroup
                 }
                 if ($TemplateParameterFilePath) {
                     $parameters.TemplateParameterFile = $TemplateParameterFilePath
