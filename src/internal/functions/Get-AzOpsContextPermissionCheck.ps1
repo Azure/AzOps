c40db325d1b5ecb7d8fe848c0a78af6b4b693c45
SomilGanguly <Somil.dodo@gmail.com>
2021-10-06 10:51:59 +0000

    Added Role Check Generic Function

---
+++ src/internal/functions/Get-AzOpsContextPermissionCheck.ps1
@@ -0,0 +1,64 @@
+function Get-AzOpsContextPermissionCheck {
+
+    <#
+        .SYNOPSIS
+            Validates if context has permission specified in validatePermissionList.
+        .DESCRIPTION
+            Validates if context has permission specified in validatePermissionList.
+        .PARAMETER contextObjectId
+            The ObjectId of the Context SPN
+        .PARAMETER scope
+            Scope of the resource
+        .PARAMETER validatePermissionList
+            The permission list to perform operation.
+        .EXAMPLE
+            > Get-AzOpsContextPermissionCheck -contextObjectId $contextObjectId -scope $scope -validatePermissionList $validatePermissionList
+            Validates if context contains anyone of permission mentioned in validatePermissionList
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        $contextObjectId,
+
+        [Parameter(Mandatory = $true)]
+        $scope,
+
+        [Parameter(Mandatory = $true)]
+        $validatePermissionList
+    )
+
+    process {
+        $roleAssignmentPermissionCheck=$false
+        $token = (Get-AzAccessToken).Token
+        $requestHeader = @{
+            "Authorization" = "Bearer " + $token
+            "Content-Type" = "application/json"
+        }
+        $uri = "https://management.azure.com$scope/providers/Microsoft.Authorization/roleAssignments?api-version=2015-07-01"
+        $roleAssignmentList = (Invoke-RestMethod -Method GET -Headers $requestheader -Uri $uri)
+        foreach($role in $roleAssignmentList.value.properties)
+        {
+            if ($scope.contains("/subscriptions")) {
+                if(-not ($role.scope -eq $scope -or $role.scope -eq '/'))
+                {
+                    Continue
+                }
+            }
+            if($role.principalId -eq $contextObjectId)
+            {
+                foreach ($item in $validatePermissionList) {
+                $roledefinitionId = $role.roleDefinitionId.Substring($role.roleDefinitionId.LastIndexOf('/') +1)
+                    if(Get-AzRoleDefinition -Id $roledefinitionId | Where-Object {$_.Actions -contains $item -or $_.Actions -eq "*"}){
+                        $roleAssignmentPermissionCheck = $true
+                        break
+                    }
+                }
+                if($roleAssignmentPermissionCheck -eq $true){
+                    break
+                }
+            }
+        }
+        return $roleAssignmentPermissionCheck
+    }
+}
--- src/internal/functions/Remove-AzOpsDeployment.ps1
+++ src/internal/functions/Remove-AzOpsDeployment.ps1
@@ -30,13 +30,11 @@ function Remove-AzOpsDeployment {
         $fileItem = Get-Item -Path $TemplateFilePath
         $deploymentName = $fileItem.BaseName -replace '\.json$' -replace ' ', '_'
         $deploymentName = "AzOps-RemoveResource-$deploymentName"
-        
+        $roleAssignmentPermissionCheck = $false
         Write-PSFMessage -String 'Remove-AzOpsDeployment.Processing' -StringValues $deploymentName, $TemplateFilePath -Target $TemplateFilePath
-
         #region Parse Content
         $templateContent = Get-Content $TemplateFilePath | ConvertFrom-Json -AsHashtable
         #endregion
-
         #region Validate it is AzOpsgenerated template
         if($templateContent.metadata._generator.name -eq "AzOps")
         {
@@ -48,7 +46,6 @@ function Remove-AzOpsDeployment {
             return
         }
         #endregion Validate it is AzOpsgenerated template
-
         #region Resolve Scope
         try {
             $scopeObject = New-AzOpsScope -Path $TemplateFilePath -StatePath $StatePath -ErrorAction Stop -WhatIf:$false
@@ -62,26 +59,19 @@ function Remove-AzOpsDeployment {
             return
         }
         #endregion Resolve Scope
-
         #region SetContext
         Set-AzOpsContext -ScopeObject $scopeObject
         #endregion SetContext
-
         #GetContext
         $context = Get-AzContext
-
+        $contextObjectId = (Get-AzADServicePrincipal -ApplicationId $context.Account.id).Id
         #region PolicyAssignment
         if($scopeObject.Resource -eq "policyAssignments"){
-
             #Validate
-            $roleAssignmentPermissionCheck = $false
             $policyAssignment = Get-AzPolicyAssignment -Id $scopeObject.scope -ErrorAction Continue -ErrorVariable resultsError
-            $roleAssignments = Get-AzRoleAssignment -ServicePrincipalName $context.Account.id -Scope $policyAssignment.Properties.Scope -ErrorAction Continue
-            foreach($role in $roleAssignments){
-                if ( $role.RoleDefinitionName -eq "Owner" -or $role.RoleDefinitionName -eq "User Access Administrator" -or
-                (Get-AzRoleDefinition -Name $role.RoleDefinitionName | Where-Object {$_.Actions -contains "Microsoft.Authorization/policyAssignments/delete" -or $_.Actions -contains "Microsoft.Authorization/policyAssignments/*" -or $_.Actions -contains "Microsoft.Authorization/*"})){
-                    $roleAssignmentPermissionCheck = $true
-                }
+            if($policyAssignment){
+                $validatePermissionList = @("Microsoft.Authorization/policyAssignments/delete","Microsoft.Authorization/policyAssignments/*","Microsoft.Authorization/*")
+                $roleAssignmentPermissionCheck = Get-AzOpsContextPermissionCheck -contextObjectId $contextObjectId -scope $policyAssignment.Properties.Scope -validatePermissionList $validatePermissionList
             }
             if (($resultsError -ne $null) -or (-not $policyAssignment)) {
                 Write-PSFMessage -Level Warning -String 'Remove-AzOpsDeployment.RemovePolicyAssignment.NoPolicyAssignmentFound' -StringValues $scopeObject.Scope,$resultsError -Target $scopeObject
@@ -101,7 +91,6 @@ function Remove-AzOpsDeployment {
                 Write-PSFMessage -Level Verbose -String 'Set-AzOpsWhatIfOutput.WhatIfFile' -Target $scopeObject
                 Set-AzOpsWhatIfOutput -results $results -removeAzOpsFlag $true
             }
-
             #removal of resource
             if ($PSCmdlet.ShouldProcess("RemovePolicyAssignment?")) 
             {
@@ -112,25 +101,18 @@ function Remove-AzOpsDeployment {
             }
         }
         #endregion PolicyAssignment
-
         #Region roleAssignments
         if($scopeObject.Resource -eq "roleAssignments")
         {
             #Validate
-            $roleAssignmentPermissionCheck = $false
             $scopeOfRoleAssignment = $scopeObject.scope
             $scopeOfRoleAssignment = $scopeOfRoleAssignment.Substring(0,$scopeOfRoleAssignment.LastIndexOf('/providers'))
             $roleAssignment = Get-AzRoleAssignment -ObjectId $templateContent.resources[0].properties.PrincipalId -RoleDefinitionName $templateContent.resources[0].properties.RoleDefinitionName -scope $scopeOfRoleAssignment -ErrorAction Continue -ErrorVariable roleAssignmentError
-            
-            $roleAssignments = Get-AzRoleAssignment -ServicePrincipalName $context.Account.id -Scope $roleAssignment.Scope -ErrorAction Continue
-            foreach($role in $roleAssignments){
-                if ( $role.RoleDefinitionName -eq "Owner" -or $role.RoleDefinitionName -eq "User Access Administrator" -or
-                (Get-AzRoleDefinition -Name $role.RoleDefinitionName | Where-Object {$_.Actions -contains "Microsoft.Authorization/roleAssignments/delete" -or $_.Actions -contains "Microsoft.Authorization/roleAssignments/*" -or $_.Actions -contains "Microsoft.Authorization/*"}))
-                {
-                    $roleAssignmentPermissionCheck = $true
-                }
+            if($roleAssignment)
+            {
+                $validatePermissionList = @("Microsoft.Authorization/roleAssignments/delete","Microsoft.Authorization/roleAssignments/*","Microsoft.Authorization/*")
+                $roleAssignmentPermissionCheck = Get-AzOpsContextPermissionCheck -contextObjectId $contextObjectId -scope $roleAssignment.Scope -validatePermissionList $validatePermissionList
             }
-
             if (($roleAssignmentError -ne $null) -or (-not $roleAssignment)) {
                 Write-PSFMessage -Level Warning -String 'Remove-AzOpsDeployment.RemoveRoleAssignment.NoRoleAssignmentFound' -StringValues $scopeObject.Scope,$resultsError -Target $scopeObject
                 $results = '{0}: What if Failed: Performing the operation Removing role assignment for AD object {1} on scope {2} with role definition {3} on target {1}' -f $deploymentName,$templateContent.resources[0].properties.PrincipalId,$roleAssignment.Scope,$templateContent.resources[0].properties.RoleDefinitionName
@@ -149,7 +131,6 @@ function Remove-AzOpsDeployment {
                 Write-PSFMessage -Level Verbose -String 'Set-AzOpsWhatIfOutput.WhatIfFile' -Target $scopeObject
                 Set-AzOpsWhatIfOutput -results $results -removeAzOpsFlag $true
             }
-
             #Remove of Resource
             if ($PSCmdlet.ShouldProcess("RemoveRoleAssignment?")) {
                 Remove-AzRoleAssignment -ObjectId $templateContent.resources[0].properties.PrincipalId -RoleDefinitionName $templateContent.resources[0].properties.RoleDefinitionName -Scope $roleAssignment.Scope -ErrorAction Stop
