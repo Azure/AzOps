003d0cb19746bc16764df50721a6cbfd3bd039c8
uday31in <14359777+uday31in@users.noreply.github.com>
2021-10-11 00:31:57 +0100

    Bugfix for AzOpsRoleDefinition when assignment scope contain trailing '/'

--- src/internal/classes/AzOpsRoleDefinition.ps1
+++ src/internal/classes/AzOpsRoleDefinition.ps1
@@ -3,9 +3,10 @@
     [string]$Name
     [string]$Id
     [hashtable]$Properties
-    
     AzOpsRoleDefinition($Properties) {
-        $this.Id = $Properties.AssignableScopes[0] + '/providers/Microsoft.Authorization/roleDefinitions/' + $Properties.Id
+        # Removing the Trailing slash to ensure that '/' is not appended twice when adding '/providers/xxx'.
+        # Example: '/subscriptions/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/' is a valid assignment scope.
+        $this.Id = '/' + $Properties.AssignableScopes[0].Trim('/') + '/providers/Microsoft.Authorization/roleDefinitions/' + $Properties.Id
         $this.Name = $Properties.Id
         $this.Properties = [ordered]@{
             AssignableScopes = @($Properties.AssignableScopes)
