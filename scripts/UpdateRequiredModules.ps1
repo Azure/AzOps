0074db226625ba4e3895d3743a97d0e7a924c436
Jesper Fajers <jesper.fajers@microsoft.com>
2021-11-09 22:40:28 +0000

    updatedependencies

--- scripts/UpdateRequiredModules.ps1
+++ scripts/UpdateRequiredModules.ps1
@@ -2,21 +2,27 @@ $data = Import-PowerShellDataFile -Path "./src/AzOps.psd1"
 $modules = @()
 $existingmodule = @()
 $newmodule = @()
+$updated = $false
 $data.RequiredModules | ForEach-Object {
     $moduleVersion = $_.RequiredVersion
     $galleryVersion = (Find-Module -Name $_.ModuleName).Version
     if ($moduleVersion -lt $galleryVersion) {
         $newmodule = @{
-            "ModuleName"     = $_.ModuleName
+            "ModuleName" = $_.ModuleName
             "RequiredVersion" = $galleryVersion
         }
+        $updated = $true
         $modules += $newmodule
-    }else {
+    } else {
         $existingmodule = @{
-            "ModuleName"     = $_.ModuleName
+            "ModuleName" = $_.ModuleName
             "RequiredVersion" = $moduleVersion
         }
         $modules += $existingmodule
     }
 }
-Update-ModuleManifest -Path "./src/AzOps.psd1" -RequiredModules $modules
+if ($updated -eq $true) {
+    Update-ModuleManifest -Path "./src/AzOps.psd1" -RequiredModules $modules
+    $filePath = Join-Path -Path "/tmp" -ChildPath "updates.json"
+    ConvertTo-Json $modules | Out-File -FilePath $filePath
+}
