4a2884f443c7f5da590181cf5fccf5159df2bb64
RakshanaShanmugam <88823550+RakshanaShanmugam@users.noreply.github.com>
2022-05-12 10:33:59 +0530

    Update deploy.ps1

--- src/tests/functional/Microsoft.Network/connections/deploy/deploy.ps1
+++ src/tests/functional/Microsoft.Network/connections/deploy/deploy.ps1
@@ -1,1 +1,12 @@
+$script:runtimePath = $PSScriptRoot
+$script:deployTemplate = "deploy.json"
+$script:deployTemplateParameter = "deploy.parameters.json"
+$script:scope = "ResourceGroup"
 
+try {
+    New-AzOpsTestsDeploymentHelper -RuntimePath $script:runtimePath -Scope $script:scope -DeployTemplateFileName $script:deployTemplate -DeployTemplateParameterFileName $script:deployTemplateParameter
+}
+catch {
+    Write-PSFMessage -Level Critical -Message "Deployment failed" -Exception $_.Exception
+    throw
+}
