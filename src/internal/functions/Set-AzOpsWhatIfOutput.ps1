180a151179c50b1511c6d62dca815b5a4df185ec
Jesper Fajers <jesper@fajers.se>
2022-07-01 09:23:32 +0200

    Set-AzOpsWhatIfOutput limit calc update (#665)
    * UpdateToLogic
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- src/internal/functions/Set-AzOpsWhatIfOutput.ps1
+++ src/internal/functions/Set-AzOpsWhatIfOutput.ps1
@@ -5,11 +5,19 @@
             Logs the output from a What-If deployment
         .DESCRIPTION
             Logs the output from a What-If deployment
-        .PARAMETER results
+        .PARAMETER Results
             The WhatIf result from a deployment
+        .PARAMETER RemoveAzOpsFlag
+            RemoveAzOpsFlag is set to true when a need to push content about deletion is required
+        .PARAMETER ResultSizeLimit
+            The character limit allowed for comments 64,000
+        .PARAMETER ResultSizeMaxLimit
+            The maximum upper character limit allowed for comments 64,600
+        .PARAMETER StatePath
+            File in scope of WhatIf
         .EXAMPLE
-            > Set-AzOpsWhatIfOutput -results $results -removeAzOpsFlag $true
-            $removeAzOpsFlag is set to true when we need to push contents for Remove-AzopsDeployment to PR
+            > Set-AzOpsWhatIfOutput -Results $results
+            > Set-AzOpsWhatIfOutput -Results $results -RemoveAzOpsFlag $true
     #>
 
     [CmdletBinding()]
@@ -24,42 +32,59 @@
         $ResultSizeLimit = "64000",
 
         [Parameter(Mandatory = $false)]
-        $TemplatePath
+        $ResultSizeMaxLimit = "64600",
+
+        [Parameter(Mandatory = $false)]
+        $StatePath
     )
 
     process {
         Write-PSFMessage -Level Verbose -String 'Set-AzOpsWhatIfOutput.WhatIfFile'
-
         if (-not (Test-Path -Path '/tmp/OUTPUT.md')) {
             New-Item -Path '/tmp/OUTPUT.md' -WhatIf:$false
             New-Item -Path '/tmp/OUTPUT.json' -WhatIf:$false
         }
-        if ($TemplatePath -match '/') {
-            $TemplatePath = ($TemplatePath -split '/')[-1]
+        if ($StatePath -match '/') {
+            $StatePath = ($StatePath -split '/')[-1]
         }
-        if ($RemoveAzOpsFlag) {
-            $mdOutput = '{0}WhatIf Results for Resource Deletion of {2}:{0}```{0}{1}{0}```' -f [environment]::NewLine, $Results, $TemplatePath
+        # Measure input $Results.Changes content
+        $resultJson = ($Results.Changes | ConvertTo-Json -Depth 100)
+        $resultString = $Results | Out-String
+        $resultStringMeasure = $resultString | Measure-Object -Line -Character -Word
+        # Measure current /tmp/OUTPUT.md content
+        $existingContentMd = Get-Content -Path '/tmp/OUTPUT.md' -Raw
+        $existingContentStringMd = $existingContentMd | Out-String
+        $existingContentStringMeasureMd = $existingContentStringMd | Measure-Object -Line -Character -Word
+        # Gather current /tmp/OUTPUT.json content
+        $existingContent = @(Get-Content -Path '/tmp/OUTPUT.json' -Raw | ConvertFrom-Json -Depth 100)
+        # Check if $existingContentStringMeasureMd and $resultStringMeasure exceed allowed size in $ResultSizeLimit
+        if (($existingContentStringMeasureMd.Characters + $resultStringMeasure.Characters) -gt $ResultSizeLimit) {
+            Write-PSFMessage -Level Warning -String 'Set-AzOpsWhatIfOutput.WhatIfFileMax' -StringValues $ResultSizeLimit
+            $mdOutput = 'WhatIf Results for {1}:{0} WhatIf is too large for comment field, for more details look at PR files to determine changes.' -f [environment]::NewLine, $StatePath
         }
         else {
-            $resultJson = ($Results.Changes | ConvertTo-Json -Depth 100)
-            $resultString = $Results | Out-String
-            $resultStringMeasure = $resultString | Measure-Object -Line -Character -Word
-            if ($($resultStringMeasure.Characters) -gt $ResultSizeLimit) {
-                $mdOutput = 'WhatIf Results for {1}:{0} WhatIf is too large for comment field, for more details look at PR files to determine changes.' -f [environment]::NewLine, $TemplatePath
+            if ($RemoveAzOpsFlag) {
+                $mdOutput = '{0}WhatIf Results for Resource Deletion of {2}:{0}```{0}{1}{0}```' -f [environment]::NewLine, $Results, $StatePath
             }
             else {
-                $mdOutput = 'WhatIf Results for {2}:{0}```{0}{1}{0}```{0}' -f [environment]::NewLine, $resultString, $TemplatePath
+                if ($existingContent.count -gt 0) {
+                    $existingContent += $results.Changes
+                    $existingContent = $existingContent | ConvertTo-Json -Depth 100
+                }
+                else {
+                    $existingContent = $resultJson
+                }
+                $mdOutput = 'WhatIf Results for {2}:{0}```{0}{1}{0}```{0}' -f [environment]::NewLine, $resultString, $StatePath
+                Write-PSFMessage -Level Verbose -String 'Set-AzOpsWhatIfOutput.WhatIfFileAddingJson'
+                Set-Content -Path '/tmp/OUTPUT.json' -Value $existingContent -WhatIf:$false
             }
-            $existingContent = @(Get-Content -Path '/tmp/OUTPUT.json' -Raw | ConvertFrom-Json)
-            if ($existingContent.count -gt 0) {
-                $existingContent += $results.Changes
-                $existingContent = $existingContent | ConvertTo-Json -Depth 100
-            }
-            else {
-                $existingContent = $resultJson
-            }
-            Set-Content -Path '/tmp/OUTPUT.json' -Value $existingContent -WhatIf:$false
         }
-        Add-Content -Path '/tmp/OUTPUT.md' -Value $mdOutput -WhatIf:$false
+        if ((($mdOutput | Measure-Object -Line -Character -Word).Characters + $existingContentStringMeasureMd.Characters) -le $ResultSizeMaxLimit) {
+            Write-PSFMessage -Level Verbose -String 'Set-AzOpsWhatIfOutput.WhatIfFileAddingMd'
+            Add-Content -Path '/tmp/OUTPUT.md' -Value $mdOutput -WhatIf:$false
+        }
+        else {
+            Write-PSFMessage -Level Warning -String 'Set-AzOpsWhatIfOutput.WhatIfMessageMax' -StringValues $ResultSizeMaxLimit
+        }
     }
 }
