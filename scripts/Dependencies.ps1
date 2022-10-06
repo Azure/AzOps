8a9d8e2573294078321bd12c1befe03bf38877e4
Jesper Fajers <jesper@fajers.se>
2022-10-06 12:44:43 +0200

    Update Dependencies.ps1

--- scripts/Dependencies.ps1
+++ scripts/Dependencies.ps1
@@ -8,15 +8,21 @@ Set-PSRepository -Name $Repository -InstallationPolicy Trusted
 $modules = @("Pester", "PSModuleDevelopment", "PSScriptAnalyzer")
 Write-Host "Installing development modules"
 foreach ($module in $modules) {
+    Write-Host "Installing: $module"
     Install-Module $module -Repository $Repository -Force
 }
-
 # Runtime Modules
 $data = Import-PowerShellDataFile -Path "$PSScriptRoot/../src/AzOps.psd1"
 Write-Host "Installing runtime modules"
 foreach ($dependency in $data.RequiredModules) {
-    $module = Get-Module -Name $dependency -ListAvailable
-    if ($null -ne $module) { Uninstall-Module -Name $dependency -Force }
+    $module = Get-Module -Name $dependency.ModuleName -ListAvailable
+    if ($module) {
+        foreach ($item in $module) {
+        Write-Host "Cleanup of: $($item.Name)"
+        Uninstall-Module -Name $item.Name -Force
+        }
+    }
+    Write-Host "Installing: $($dependency.ModuleName) $($dependency.RequiredVersion)"
     Install-Module -Name $dependency.ModuleName -RequiredVersion $dependency.RequiredVersion -Repository $Repository
 }
 # Download and add bicep to PATH
