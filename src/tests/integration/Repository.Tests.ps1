2ece121091d9c122b4b9a8e65845383b2d0c6577
Jesper Fajers <jesper@fajers.se>
2023-03-16 15:41:47 +0100

    Fix regression of Subscription Only Discovery with no Management Group (#778)
    * Update SubOnly
    
    * Update
    
    * Update

--- src/tests/integration/Repository.Tests.ps1
+++ src/tests/integration/Repository.Tests.ps1
@@ -178,7 +178,7 @@ Describe "Repository" {
         }
         #endregion GeneratedRoot Pull
 
-        # The following values are discovering the file system paths so that they can be validate against ensuring that the data model behaves as intended.
+        # The following values are discovering the file system paths, ensuring that the model behaves as intended.
 
         #region Paths
         Write-PSFMessage -Level Debug -Message "GeneratedRootPath: $generatedRoot" -FunctionName "BeforeAll"
