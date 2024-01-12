172482b81a15e26289433b719bf339c19dcee53a
Jesper Fajers <jesper@fajers.se>
2024-01-12 17:27:55 +0000

    Update

--- src/internal/functions/Get-AzOpsResourceDefinition.ps1
+++ src/internal/functions/Get-AzOpsResourceDefinition.ps1
@@ -364,7 +364,7 @@
                         foreach ($exportResource in $exportResources) {
                             if (-not(($resource.name -eq $exportResource.name) -and ($resource.type -eq $exportResource.type))) {
                                 & $azOps {
-                                    Write-AzOpsMessage -LogLevel Verbose -LogString 'Get-AzOpsResourceDefinition.Processing.ChildResource' -LogStringValues $exportResource.name, $resource.resourceGroup -FunctionName "Get-AzOpsResourceDefinition" -Target $exportResource
+                                    Write-AzOpsMessage -LogLevel Verbose -LogString 'Get-AzOpsResourceDefinition.Processing.ChildResource' -LogStringValues $exportResource.name, $resource.resourceGroup -FunctionName "Get-AzOpsResourceDefinition" -ModuleName "AzOps" -Target $exportResource
                                 }
                                 $ChildResource = @{
                                     resourceProvider = $exportResource.type -replace '/', '_'
@@ -383,7 +383,7 @@
                     }
                     catch {
                         & $azOps {
-                            Write-AzOpsMessage -LogLevel Warning -LogString 'Get-AzOpsResourceDefinition.ChildResource.Warning' -LogStringValues $resource.resourceGroup, $_ -FunctionName "Get-AzOpsResourceDefinition"
+                            Write-AzOpsMessage -LogLevel Warning -LogString 'Get-AzOpsResourceDefinition.ChildResource.Warning' -LogStringValues $resource.resourceGroup, $_ -FunctionName "Get-AzOpsResourceDefinition" -ModuleName "AzOps"
                         }
                     }
                     if (Test-Path -Path $tempExportPath) {
