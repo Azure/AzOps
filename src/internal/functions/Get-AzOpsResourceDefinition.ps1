3256510e04df8bc7562313a13c0da8a4adbd4f9a
Jesper Fajers <jesper@fajers.se>
2025-03-12 13:34:59 +0000

    Update

--- src/internal/functions/Get-AzOpsResourceDefinition.ps1
+++ src/internal/functions/Get-AzOpsResourceDefinition.ps1
@@ -353,7 +353,7 @@
                                 param (
                                     $ExportParameters
                                 )
-                                $param = $ExportParameters
+                                $param = $ExportParameters | Write-Output
                                 Export-AzResourceGroup @param -Confirm:$false -Force -ErrorAction Stop | Out-Null
                             } -RetryCount $runspaceData.MaxRetryCount -RetryWait $runspaceData.BackoffMultiplier -RetryType Exponential
                         }
